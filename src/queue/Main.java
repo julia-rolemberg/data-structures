01e63feb05562a035de11a16225f0083afb4fed3
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-06-21 12:13:32 -0300

    Queue tests

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -1,4 +1,26 @@
 package queue;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+
+        //front element
+        System.out.println("First element: "+queue.front());
+
+        //element on third position
+        System.out.println("Element on third position: "+queue.get(3));
+
+        queue.print();
+
+        System.out.println("\nSize: "+queue.size());
+
+
+    }
+
 }
